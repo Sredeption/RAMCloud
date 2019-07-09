cb4ef7a5bc782c3a99233f3933eadfccb5c0ea6a
Your Name <you@example.com>
2019-07-09 16:51:05 -0400

    enable priority pull in fulva

--- run-fulva.sh
+++ run-fulva.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+    # enum TabletState {
+    #     /// The tablet is available.
+    #     NORMAL = 0,
+    #     /// The tablet is being re-constructed yet. (eg. migration and recovery)
+    #     NOT_READY = 1,
+    #     /// Migration of tablet is requested. Cannot take new writes.
+    #     LOCKED_FOR_MIGRATION = 2,
+    #     /// The tablet is being migrated over by rocksteady. It can take new
+    #     /// writes and reads.
+    #     ROCKSTEADY_MIGRATING = 3,
+    # };
+    
+    # In rocksteady, the target tablet is in ROCKSTEADY_MIGRATING state
+    # the source tablet is in LOCKED_FOR_MIGRATION state
+
 python ./scripts/backupMigration.py -r 0 --servers=4 --clients=1 --dpdkPort=0 -T basic+dpdk --superuser
